d7205ff26300d3e780c6dfbc10bb7b6c05c71007
Stanislav <stassssssssssssss13@gmail.com>
2026-04-06 13:59:15 +0300

    Добавил админки к моделям

--- PokerWEB/Game/admin.py
+++ PokerWEB/Game/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Tournament, Season
+
+
 # Register your models here.
+@admin.register(Tournament)
+class TournamentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'season')
+
+
+@admin.register(Season)
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'date_start', 'date_end')
--- PokerWEB/History/admin.py
+++ PokerWEB/History/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import TournamentResult
+
+
 # Register your models here.
+@admin.register(TournamentResult)
+class TournamentResultAdmin(admin.ModelAdmin):
+    list_display = ('id', 'tournament', 'user')
